b49ec03a7cefa8d3951c95f02de23b2a1851bdb7
HugOtavioS <hugotavio2005@gmail.com>
2025-01-10 10:46:17 -0300

    Login
    Sistema de login feito, mas ainda não completo

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Controllers;
+
+use App\Controllers\ViewController;
+use Config\database;
+use Config\env;
+use App\Request;
+use App\Session;
+
+class LoginController {
+    private $view;
+    private $db;
+    private $request;
+    private $session;
+
+    public function __construct () {
+
+        $this->view = new ViewController();
+        $this->db = new database(new env);
+        $this->request = new Request();
+        $this->session = new Session();
+
+    }
+    public function index() {
+
+        $this->view->load("login", ["title" => "Login"]);
+
+    }
+
+    public function create () {
+
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        $this->session->init();
+        if (count($this->db->select(["*"],"users", ["email" => $email, "senha" => $senha], "AND")) > 0) {
+
+            if (!$this->session->get("session_login")) {
+                $this->session->set("sesseion_login", "$email");
+            }
+
+            echo "Usuário Encontrado!";
+
+        } else {
+
+            $this->request->redirect("http:\\login?error");
+
+        }
+
+    }
+}
--- app/Routes/web.php
+++ app/Routes/web.php
@@ -1,7 +1,9 @@
 <?php
 use App\Routes\Router;
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
 
 Router::addRoute("/", "GET", HomeController::class, "index");
-Router::addRoute("/contato", "GET", HomeController::class, "index");
-Router::addRoute("/produtos", "GET",  HomeController::class, "index");
+Router::addRoute("/login", "GET", LoginController::class, "index");
+Router::addRoute("/login?error", "GET", LoginController::class, "index");
+Router::addRoute("/createlogin", "POST", LoginController::class, "create");
